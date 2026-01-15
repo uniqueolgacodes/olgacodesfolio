fb437590dfb4c9cf07d194a25d3a30a247517a84
UniqueOlga <flamezthrower49@gmail.com>
2026-01-16 00:27:51 +0100

    changing hamish url

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -5,7 +5,7 @@ addons.setConfig({
   theme: {
     ...themes.dark,
     brandImage: './icon.svg',
-    brandTitle: 'Hamish Williams Components',
-    brandUrl: 'https://hamishw.com',
+    brandTitle: 'Olgacodes Components',
+    brandUrl: 'https://olgacodes.netlify.app',
   },
 });
